a3fbf9a80e6cbd9c4df49c592b3ca0e18261579c
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-05-16 10:11:03 +0800

    Add vue router

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <header class="header">
     <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
   </header>
 </template>
 
@@ -16,5 +20,19 @@ export default {
   color: #fff;
   text-align: center;
   padding: 10px;
+}
+
+#nav {
+  padding: 5px 10px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #fff;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. It is part of the Vue Crash Course on YouTube.</p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  padding: 10px;
+}
+</style>
